65108308363e258982f63dac32989daec56f9e44
RMABID <mdabidaliimran@gmail.com>
2025-01-14 20:58:55 +0600

    my product table formate data show

---
+++ src/components/Dashboard/Tables/UserProductTable.jsx
@@ -0,0 +1,30 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FiEdit } from "react-icons/fi";
+
+const UserProductTable = ({ item }) => {
+  const { product_image, product_name, _id } = item;
+  return (
+    <tr>
+      <td>
+        <div className="mask mask-squircle h-12 w-12">
+          <img src={product_image} alt={product_name} />
+        </div>
+      </td>
+      <td>{product_name}</td>
+      <td>5</td>
+      <td>coming</td>
+      <th>
+        <div className="flex items-center justify-start">
+          <button className="btn btn-ghost ">
+            <FiEdit />
+          </button>
+          <button className="btn btn-ghost ">
+            <FaRegTrashAlt />
+          </button>
+        </div>
+      </th>
+    </tr>
+  );
+};
+
+export default UserProductTable;
--- src/pages/Dashboard/User/MyProducts.jsx
+++ src/pages/Dashboard/User/MyProducts.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import UserProductTable from "../../../components/Dashboard/Tables/UserProductTable";
 
 const MyProducts = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: products = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["products", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/products/${user?.email}`);
+      return data;
+    },
+  });
+  console.log(products);
   return (
-    <div>MyProducts</div>
-  )
-}
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Votes</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((item, index) => (
+              <UserProductTable key={index} item={item} />
+            ))}
+          </tbody>
+          \
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default MyProducts
+export default MyProducts;
