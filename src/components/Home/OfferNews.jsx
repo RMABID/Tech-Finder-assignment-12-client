c976e3317085df0e7677a23ffaf951f545321f5f
RMABID <mdabidaliimran@gmail.com>
2025-02-19 16:06:42 +0600

    offer news updated

--- src/components/Home/OfferNews.jsx
+++ src/components/Home/OfferNews.jsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
-// import "./swiper.css";
 import { EffectCards } from "swiper/modules";
 import { Link } from "react-router-dom";
 import slider_imag from "../../assets/img/squid game.jpeg";
